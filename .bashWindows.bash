e3fad54b0594a9078f349c8a383122aaa595a206
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-06-27 16:26:12 +0900

    update windows $TERM setting

--- .bashWindows.bash
+++ .bashWindows.bash
@@ -2,6 +2,7 @@
 
 alias idea='"/C/Program Files/JetBrains/IntelliJ IDEA 2018.1.4/bin/idea64.exe"'
 
+# hyperの設定が原因だったようなのでコメントアウト
 # fzfがcharacter set not supportedになってしまうのを防ぐ
-TERM=cygwin
+#TERM=cygwin
 
